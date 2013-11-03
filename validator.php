a1ddf7479bb9198f55764665c9b3e4f0b541ea89
Harris Weeks <h.weeks@gmail.com>
2013-11-03 09:42:48 -0500

    Error Handling
    Added my error array, but I haven't quite come up with how I want to
    handle passing multiple errors at once. I am thinking about just adding
    to the original error. We'll see what happens.

--- validator.php
+++ validator.php
@@ -13,34 +13,37 @@ $body_message .= 'Password (Which should totally never be sent in plaintext): '.
 
 $mail_status = mail($mail_to, $subject, $body_message);
 
-if(!filter_var($field_email, FILTER_VALIDATE_EMAIL)){ 		
+$errors = [];
 
+if(!filter_var($field_email, FILTER_VALIDATE_EMAIL)){ 		
+$errors['email'][] = 'Email invalid, please check and resubmit.';
 }
 
 if (strlen($field_password) < 8) {
-
+$errors['password'][0] = 'Password must be at least 8 characters.';
 }
 
 if (!preg_match("#[0-9]+#", $field_password)) {
-
+$errors['password'][0] = 'Password must contain at least a number.';
 }
 
 if (!preg_match("#[a-zA-Z]+#", $field_password)) {
-
+$errors['password'][0] = 'Password must contain at least a letter.';
 } 
 
 $field_user_parsed = str_replace('.', '', $field_user);
 
 if (!ctype_lower($field_user_parsed)) {
-
+$errors['user'][0] = 'Username must be lower case.';
 }
 
 if (substr_count($field_user, '.') > 1) {
-
+$errors['user'][0] = 'Username cannot contain more that one period.';
 }
 
 if (!empty($errors)) {
-
+$_SESSION['errors'] = $errors;
+header('Location: /xampp/party/index.php');
 }
 
 else{
@@ -48,6 +51,6 @@ session_destroy();
 }
 
 if ($mail_status) {
-
+header('Location: /xampp/party/index.php');
 }
 ?>
