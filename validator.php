db403c172dc406c36f2ca34d092dc0d398391bed
Harris Weeks <h.weeks@gmail.com>
2013-11-04 00:02:44 -0500

    Final Functional Commit
    I have gotten validation working, turns out the sandbow runs PHP 5.3,
    not my 5.4 so arrays must be called longhand. Got that fixed. I also did
    the @media in the css of any device 500px wide and under. Most modern
    smart phones are plenty big enough for the normal web, so it only really
    matters for throw back smart phones now. I also created styles for said
    mobile phones, and it's off to browser stack to test.

--- validator.php
+++ validator.php
@@ -13,37 +13,58 @@ $body_message .= 'Password (Which should totally never be sent in plaintext): '.
 
 $mail_status = mail($mail_to, $subject, $body_message);
 
-$errors = [];
+$errors = array();
 
 if(!filter_var($field_email, FILTER_VALIDATE_EMAIL)){ 		
 $errors['email'][] = 'Email invalid, please check and resubmit.';
+
 }
 
 if (strlen($field_password) < 8) {
-$errors['password'][0] = 'Password must be at least 8 characters.';
+if (strlen($errors['password'][0] < 10)){	
+$errors['password'][0] = 'Password must ';
+}	
+$errors['password'][0] = $errors['password'][0].' be at least 8 characters ';
+
 }
 
 if (!preg_match("#[0-9]+#", $field_password)) {
-$errors['password'][0] = 'Password must contain at least a number.';
+if (strlen($errors['password'][0] > 10)){	
+$errors['password'][0] = $errors['password'][0].' and ';
+}	
+if (strlen($errors['password'][0] < 10)){	
+$errors['password'][0] = 'Password must ';
+}	
+$errors['password'][0] = $errors['password'][0].' have numbers ';
 }
 
 if (!preg_match("#[a-zA-Z]+#", $field_password)) {
-$errors['password'][0] = 'Password must contain at least a letter.';
+if (strlen($errors['password'][0] > 10)){	
+$errors['password'][0] = $errors['password'][0].' and ';
+}
+
+if (strlen($errors['password'][0] < 10)){	
+$errors['password'][0] = 'Password must ';
+}	
+$errors['password'][0] = $errors['password'][0].' have letters ';
 } 
 
 $field_user_parsed = str_replace('.', '', $field_user);
 
 if (!ctype_lower($field_user_parsed)) {
 $errors['user'][0] = 'Username must be lower case.';
+
 }
 
 if (substr_count($field_user, '.') > 1) {
 $errors['user'][0] = 'Username cannot contain more that one period.';
+
 }
 
 if (!empty($errors)) {
 $_SESSION['errors'] = $errors;
 header('Location: /xampp/party/index.php');
+
 }
 
 else{
@@ -52,5 +73,6 @@ session_destroy();
 
 if ($mail_status) {
 header('Location: /xampp/party/index.php');
+
 }
 ?>
